4c812db79de08196ce2dba5f0648e30b4861336c
Sean Hammond <git@snhmnd.fastmail.com>
2020-02-03 13:16:27 +0000

    Remove some unnecessary code

--- lms/static/scripts/frontend_apps/components/BasicLtiLaunchApp.js
+++ lms/static/scripts/frontend_apps/components/BasicLtiLaunchApp.js
@@ -71,7 +71,6 @@ export default function BasicLtiLaunchApp() {
     context_title,
     custom_canvas_course_id,
     student_custom_canvas_user_id,
-    student_lti_user_id,
   } = useContext(Config);
 
   const [ltiLaunchState, setLtiLaunchState] = useState({
@@ -111,7 +110,6 @@ export default function BasicLtiLaunchApp() {
           'context_title': context_title,
           'custom_canvas_course_id': custom_canvas_course_id,
           'student_custom_canvas_user_id': student_custom_canvas_user_id,
-          'student_lti_user_id': student_lti_user_id,
       }
       const group_ids = await apiCall({
         authToken,
